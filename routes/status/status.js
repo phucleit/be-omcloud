70e74d3162c24b313ea96a70753f6804a87efcb2
phucleit <phucleit95@gmail.com>
2024-09-19 22:50:58 +0700

    them phan quyen trang thai ver 2

--- routes/status/status.js
+++ routes/status/status.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
+const { check_role } = require("../../middleware/middleware_role");
 const statusController = require("../../controllers/status/controller");
 
-router.post("/", statusController.addStatus);
+router.post("/", check_role("667467eb263fb998b9925d3a"), statusController.addStatus);
 router.get("/", statusController.getStatus);
 router.get("/:id", statusController.getDetailStatus);
-router.put("/:id", statusController.updateStatus);
-router.delete("/:id", statusController.deleteStatus);
+router.put("/:id", check_role("667467eb263fb998b9925d3b"), statusController.updateStatus);
+router.delete("/:id", check_role("667467eb263fb998b9925d3c"),statusController.deleteStatus);
 
 module.exports = router;
